27f8c5c5735201bddc363f32004847ff8b90f885
tmacy <chen_tmacy@foxmail.com>
2016-03-23 14:33:09 +0800

    新文件：   gif/exercise_gif.go 	新文件：   gif/gif.go 	新文件：   gif/out.gif

---
+++ gif/exercise_gif.go
@@ -0,0 +1,63 @@
+// Lissajous generates GIF animations of random Lissajous figures.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+//var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{
+	color.White,
+	color.Black,
+	color.RGBA{0xad, 0xff, 0x2f, 0xff},
+	color.RGBA{0x7b, 0x68, 0xee, 0xff}
+}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+	greenIndex = 2 // next color in palette
+	blueIndex  = 3 // next color in palette
+
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			if int(t)%2 == 0 {
+				img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
+			} else {
+				img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blueIndex)
+			}
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
